a043d30e8bdf4c3584e2a709970620bfc70b8a7d
Sezer Ökmen <szrokmn34@gmail.com>
2022-10-17 23:14:06 +0300

    css revizion

--- javaScript.js
+++ javaScript.js
@@ -1,23 +1,3 @@
-//*=================================================
-//*              ASYNC-AWAIT
-//*=================================================
-//? Async-Await ECMAScript 2017 ile Javascript diline eklenmistir.
-//? Aslinda Promise yapisinin syntax olarak basitlestirilmis halidir.
-//? Bu baglamda sentetik seker benzetmesi yapilabilir.
-
-//* Bir fonskiyonu asenkron hale getirmek icin fonksiyon keyword'nun onune
-//* async keyword'u eklenir.
-
-//* Bir async fonksiyon icerisinde await keyword'u ile yapilan istegin cevabinin
-//* beklenmesi saglanir.
-
-//* Aslinda dizilis olarak senkron mantiga benzeyen kod yazarak Asenkron
-//* kod yazmayı mumkun kilar.
-
-//* Await, promise-temelli herhangi bir fonksiyonun onune getirilerek getirildigi
-//* satirdaki kodun durudurulmasini saglar. Yapilan istek yerine getirilip sonuc
-//* degerlerinin dondurulmesine ile kodun calismasi devam eder.
-
 let isError = false;
 const getNews = async function () {
     const apiKey = "8a4c426f938a4ee38266f2378b9247e7";
@@ -26,7 +6,7 @@ const getNews = async function () {
       const res = await fetch(url);
       if (!res.ok) {
         isError = true;
-        // throw new Error(`something went wrong ${res.status}`);
+        throw new Error(`something went wrong ${res.status}`);
       }
       const data = await res.json();
       // console.log(data.articles);
